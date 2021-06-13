aced825257b78d9e117d9cf88caea56386f79598
leonardo <leofiore@libero.it>
2021-06-13 17:03:32 +0200

    refactoring: starting with 2to3

--- id3-tagger.py
+++ id3-tagger.py
@@ -2,7 +2,7 @@
 
 # ID3 module example program
 # $Id$
-       
+
 # version 1.2
 # written 2 May 1999 by Ben Gertzfield <che@debian.org>
 
@@ -22,72 +22,72 @@ Usage: %s [-t title] [-a artist] [-A album] [-y year] [-c comment] \n\
 -d: Delete the ID3 tag from specified file(s) completely\n\
 -h: Display this text\n\
 -v: Display the version of this program\n\n\
-With no arguments, display the ID3 tag of the given file(s).\n" % 
+With no arguments, display the ID3 tag of the given file(s).\n" %
        (name, version, name, ' ' * len(name)))
 
 def main():
     options = {}
 
     try:
-	opts, args = getopt.getopt(sys.argv[1:], 't:a:A:y:c:g:T:dhvl')
-    except getopt.error, msg:
-	print msg
-	usage()
-	sys.exit(2)
+        opts, args = getopt.getopt(sys.argv[1:], 't:a:A:y:c:g:T:dhvl')
+    except getopt.error as msg:
+        print(msg)
+        usage()
+        sys.exit(2)
 
     for opt, arg in opts:
-	if opt == '-v':
-	    sys.stderr.write("This is %s version %0.1f.\n" % (name, version))
-	    sys.exit(0)
-	if opt == '-h':
-	    usage()
-	    sys.exit(0)
-	if opt == '-t':
-	    options['TITLE'] = arg
-	if opt == '-a':
-	    options['ARTIST'] = arg
-	if opt == '-A':
-	    options['ALBUM'] = arg
-	if opt == '-y':
-	    options['YEAR'] = arg
-	if opt == '-c':
-	    options['COMMENT'] = arg
-	if opt == '-g':
-	    options['GENRE'] = arg
+        if opt == '-v':
+            sys.stderr.write("This is %s version %0.1f.\n" % (name, version))
+            sys.exit(0)
+        if opt == '-h':
+            usage()
+            sys.exit(0)
+        if opt == '-t':
+            options['TITLE'] = arg
+        if opt == '-a':
+            options['ARTIST'] = arg
+        if opt == '-A':
+            options['ALBUM'] = arg
+        if opt == '-y':
+            options['YEAR'] = arg
+        if opt == '-c':
+            options['COMMENT'] = arg
+        if opt == '-g':
+            options['GENRE'] = arg
         if opt == '-T':
             options['TRACKNUMBER'] = arg
-	if opt == '-d':
-	    options['delete'] = 1
+        if opt == '-d':
+            options['delete'] = 1
 
     if len(args) == 0:
-	usage()
-	sys.exit(2)
+        usage()
+        sys.exit(2)
 
     for file in args:
-	try:
-	    id3info = ID3(file)
+        try:
+            id3info = ID3(file)
 
             needs_write = 0
 
-            if len(options.keys()) > 0:
+            if len(list(options.keys())) > 0:
                 needs_write = 1
 
-            for k, v in options.items():
+            for k, v in list(options.items()):
                 if k == 'GENRE' and re.match("\d+$", v):
                     id3info[k] = string.atoi(v)
                 else:
                     id3info[k] = v
 
-	    if options.has_key('delete'):
-		id3info.delete()
+            if 'delete' in options:
+                id3info.delete()
 
-	    print id3info
+            print(id3info)
 
             if needs_write:
                 id3info.write()
 
-	except InvalidTagError, msg:
-	    print "Invalid ID3 tag:", msg
-	    continue
+        except InvalidTagError as msg:
+            print("Invalid ID3 tag:", msg)
+            continue
 
 main()
--- setup.py
+++ setup.py
@@ -6,14 +6,13 @@ __revision__ = "$Id$"
 
 from distutils.core import setup
 
-setup (# Distribution meta-data
-       name = "ID3",
-       version = "1.2",
-       description = "Module for manipulating ID3 informational tags on MP3 audio files",
-       author = "Ben Gertzfield",
-       author_email = "che@debian.org",
-       url = "http://id3-py.sourceforge.net/",
-
-       # Description of the modules and packages in the distribution
-       py_modules = ['ID3']
-      )
+setup(  # Distribution meta-data
+    name="ID3",
+    version="1.2",
+    description="Module for manipulating ID3 informational tags on MP3 audio files",
+    author="Ben Gertzfield",
+    author_email="che@debian.org",
+    url="http://id3-py.sourceforge.net/",
+    # Description of the modules and packages in the distribution
+    py_modules=["ID3"],
+)
